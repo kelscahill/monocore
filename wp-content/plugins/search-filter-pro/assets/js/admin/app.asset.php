69fc23c2481a512801a77770b942d97484cd15ee
Kelsey Cahill <kelscahill@gmail.com>
2025-02-16 17:27:53 -0500

    Plugin updates

--- wp-content/plugins/search-filter-pro/assets/js/admin/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '4676bfad134ae524a0a8');
